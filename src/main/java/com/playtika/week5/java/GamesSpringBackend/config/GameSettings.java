12cec9a9e5cb1b78920af5d21355db17becec189
Teodora17416 <yolo17416>
2021-08-23 10:46:06 +0300

    Immutabil commit

--- src/main/java/com/playtika/week5/java/GamesSpringBackend/config/GameSettings.java
+++ src/main/java/com/playtika/week5/java/GamesSpringBackend/config/GameSettings.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Component
@@ -28,4 +29,29 @@ public class GameSettings {
     private Integer[] ports;
     @Value("#{${contribution}}")
     private Map<String,Integer> contribution;
+
+
+    public String getVersion() {
+        return version;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getMaxPlayers() {
+        return maxPlayers;
+    }
+
+    public String[] getDevelopers() {
+        return developers;
+    }
+
+    public Integer[] getPorts() {
+        return ports.clone();
+    }
+
+    public Map<String, Integer> getContribution() {
+        return new HashMap<>(this.contribution);
+    }
 }
